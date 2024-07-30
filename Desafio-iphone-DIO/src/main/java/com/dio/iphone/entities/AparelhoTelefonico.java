27653b898393f782dca56f41c5fab4177ee94c50
Renan Reis <renanmacfernandes@hotmail.com>
2024-07-30 15:56:18 -0300

    Criacao da classe AparelhoTelefonico

---
+++ Desafio-iphone-DIO/src/main/java/com/dio/iphone/entities/AparelhoTelefonico.java
@@ -0,0 +1,18 @@
+package com.dio.iphone.entities;
+
+public class AparelhoTelefonico {
+	
+	public String ligar(String numero) {
+        this.ligar(numero);
+        return "Lingandooooo!!!!";
+    }
+    
+    public String atender() {
+        return "Atendeu!";
+    }
+    
+    public String iniciarCorreioVoz() {
+        return "Caiu no correio de voz!";
+    }
+
+}
