76e13a32caf9db5084fa3c6487643454d4f3430d
Renan Reis <renanmacfernandes@hotmail.com>
2024-07-30 15:09:39 -0300

    Initial

---
+++ Desafio-iphone-DIO/src/test/java/com/dio/iphone/DesafioIphoneDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.iphone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioIphoneDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
