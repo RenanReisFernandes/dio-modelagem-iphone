87cf1ac9c8df6f09b8b1d56f0bedf2f4c6861165
Renan Reis <renanmacfernandes@hotmail.com>
2024-07-30 15:59:15 -0300

    Criacao da classe NavegadorNaInternet

---
+++ Desafio-iphone-DIO/src/main/java/com/dio/iphone/entities/NavegadorNaInternet.java
@@ -0,0 +1,18 @@
+package com.dio.iphone.entities;
+
+public class NavegadorNaInternet {
+	
+	public String exibirPagina(String url) {
+        this.exibirPagina(url);
+        return "Abriu!";
+    }
+    
+    public String adicionarNovaAba() {
+        return "Nova Aba";
+    }
+    
+    public String atualizarPagina() {
+        return "Página Atualizada ";
+    }
+
+}
